4b6eff000f4f4cfce9094582caa8621748b1fece
wanxiaodong <729779978@qq.com>
2018-11-14 16:50:17 +0800

    测试案例优化

--- src/server.js
+++ src/server.js
@@ -2,18 +2,16 @@
  * @Author: wanxiaodong
  * @Date: 2018-11-14 14:18:12
  * @Last Modified by: wanxiaodong
- * @Last Modified time: 2018-11-14 16:40:11
+ * @Last Modified time: 2018-11-14 16:47:43
  */
 const http = require('http')
 let _server = null
 module.exports = {
-    server: function (middleware) {
+    server: function (middleware, callback) {
         _server = http.createServer(function (req, res){
             middleware && middleware(req, res)
             res.end('done')
-        }).listen(8000, function() {
-            console.log('server is ready')
-        })
+        }).listen(8000, callback)
     },
     close() {
         _server && _server.close()
--- test/request.test.js
+++ test/request.test.js
@@ -2,7 +2,7 @@
  * @Author: wanxiaodong
  * @Date: 2018-11-14 15:41:21
  * @Last Modified by: wanxiaodong
- * @Last Modified time: 2018-11-14 16:42:25
+ * @Last Modified time: 2018-11-14 16:49:22
  */
 const http = require('http')
 const expect = require('chai').expect;
@@ -14,13 +14,12 @@ describe('请求获取ip地址', function () {
     beforeEach(function(done) {
         server(function() {
             ip = getIp(...arguments)
-        });
-        setTimeout(function(){
+        }, function (){
             http.get('http://127.0.0.1:8000/', function() {
                 close()
                 done()
             })
-        }, 1000)
+        });
     })
     it('获取ip地址', function() {
         expect(ip).to.match(/(\d.?){4}/)
