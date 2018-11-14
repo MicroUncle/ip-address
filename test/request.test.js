e752926dda3f7686644339b1fe5f26867d583d8e
wanxiaodong <729779978@qq.com>
2018-11-14 16:55:08 +0800

    目录调整

--- test/request.test.js
+++ test/request.test.js
@@ -2,11 +2,11 @@
  * @Author: wanxiaodong
  * @Date: 2018-11-14 15:41:21
  * @Last Modified by: wanxiaodong
- * @Last Modified time: 2018-11-14 16:49:22
+ * @Last Modified time: 2018-11-14 16:54:38
  */
 const http = require('http')
 const expect = require('chai').expect;
-const {server, close} = require('../src/server')
+const {server, close} = require('../src/demo/server')
 const getIp = require('../src/index')
 
 describe('请求获取ip地址', function () {
