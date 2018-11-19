d8645c8571985a57fa27199625f6fe6797873694
wanxiaodong <729779978@qq.com>
2018-11-19 15:17:04 +0800

    test: 调整单元测试

--- test/request.test.js
+++ test/request.test.js
@@ -2,7 +2,7 @@
  * @Author: wanxiaodong
  * @Date: 2018-11-14 15:41:21
  * @Last Modified by: wanxiaodong
- * @Last Modified time: 2018-11-19 15:15:23
+ * @Last Modified time: 2018-11-19 15:16:49
  */
 const http = require('http')
 const expect = require('chai').expect;
@@ -23,7 +23,6 @@ describe('请求获取ip地址', function () {
         });
     })
     it('获取ip地址', function() {
-        console.log(address)
         expect(address.ip).to.match(/(\d.?){4}/)
     })
 })
