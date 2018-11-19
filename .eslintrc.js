84c2fe488e35a05394a0e730184e3fec85db9c1f
wanxiaodong <729779978@qq.com>
2018-11-19 10:19:11 +0800

    build: 添加docusaurus && 修改eslint规则

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,23 @@
 module.exports = {
-    "extends": "google"
-};
+    root: true,
+    // parser: 'babel-eslint',
+    env: {
+        browser: true,
+        node: true
+    },
+    extends: 'standard',
+    // required to lint *.vue files
+    // add your custom rules here
+    rules: {
+        indent: 'off',
+        'space-before-function-paren': 'off',
+        semi: 'off',
+        'no-new': 'off',
+        'eol-last': 'off',
+        'no-eval': 'off',
+        'standard/computed-property-even-spacing': 'off',
+        'no-unreachable': 'off',
+        'one-var': 'off'
+    },
+    globals: {}
+}
