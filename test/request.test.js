650455aac7e33c11ef61a91abb503f7bfd3233e1
wanxiaodong <729779978@qq.com>
2018-11-19 15:48:34 +0800

    test: 测试覆盖率

--- test/request.test.js
+++ test/request.test.js
@@ -2,7 +2,7 @@
  * @Author: wanxiaodong
  * @Date: 2018-11-14 15:41:21
  * @Last Modified by: wanxiaodong
- * @Last Modified time: 2018-11-19 15:16:49
+ * @Last Modified time: 2018-11-19 15:25:12
  */
 const http = require('http')
 const expect = require('chai').expect;
@@ -22,7 +22,10 @@ describe('请求获取ip地址', function () {
             })
         });
     })
-    it('获取ip地址', function() {
+    it('IP地址', function() {
         expect(address.ip).to.match(/(\d.?){4}/)
+    })
+    it('地址源数据', function() {
+        expect(address.ip).to.be.a('string')
     })
 })
