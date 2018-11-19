7093454434e2df57ee22c4ec0874f04762868563
wanxiaodong <729779978@qq.com>
2018-11-19 15:15:53 +0800

    test: 调整单元测试

--- test/request.test.js
+++ test/request.test.js
@@ -2,18 +2,19 @@
  * @Author: wanxiaodong
  * @Date: 2018-11-14 15:41:21
  * @Last Modified by: wanxiaodong
- * @Last Modified time: 2018-11-14 16:54:38
+ * @Last Modified time: 2018-11-19 15:15:23
  */
 const http = require('http')
 const expect = require('chai').expect;
-const {server, close} = require('../src/demo/server')
-const getIp = require('../src/index')
+const {server, close} = require('./util/server')
+const getIp = require('../index')
 
 describe('请求获取ip地址', function () {
-    let ip;
+    let address;
     beforeEach(function(done) {
-        server(function() {
-            ip = getIp(...arguments)
+        server(function(req) {
+            getIp(...arguments)
+            address = req.address
         }, function (){
             http.get('http://127.0.0.1:8000/', function() {
                 close()
@@ -22,6 +23,7 @@ describe('请求获取ip地址', function () {
         });
     })
     it('获取ip地址', function() {
-        expect(ip).to.match(/(\d.?){4}/)
+        console.log(address)
+        expect(address.ip).to.match(/(\d.?){4}/)
     })
 })
